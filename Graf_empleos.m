07fe2a045faf9b128c88bef9dd8e29076884e346
Gust67124 <51275395+Gust67124@users.noreply.github.com>
2019-07-11 20:59:52 -0500

    Create Graf_empleos.m

---
+++ Graf_empleos.m
@@ -0,0 +1,3 @@
+numest = [140000, 130000, 120000, 120000, 120000, 110000]; 
+bar (numest); str = {'Ing y Manufactura', 'Finanzas', 'Tecnologias de la Informacion', 'Logistica y Construccion', 'Ventas y Marketing', 'Recursos Humanos', }; 
+set (gca, 'XTickLabel', str)
